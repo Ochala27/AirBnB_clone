335235fe763d5551e57d91f0a45621353b088388
Ochala27 <ochalaonuche3@gmail.com>
2023-12-10 07:58:27 -0800

    clone

--- console.py
+++ console.py
@@ -193,7 +193,7 @@ class HBNBCommand(cmd.Cmd):
             elif not attribute:
                 print("** attribute name missing **")
             elif not value:
-                print("** value missing **")
+                print("** values missing **")
             else:
                 cast = None
                 if not re.search('^".*"$', value):
